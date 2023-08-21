73fb5ba47ec53a3840034589902c596b00698936
Kaushal273 <kaushalyadav536@gmail.com>
2023-08-21 23:02:10 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {v4 as uuid} from 'uuid';
 
 //components
 import Editor from "./component/Editor";
@@ -6,9 +8,12 @@ import Editor from "./component/Editor";
 
 function App() {
   return (
-    <>
-      <Editor />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element = {<Navigate replace to={`/docs/${uuid()}`}/>} />
+        <Route path='/docs/:id' element={<Editor />} />
+      </Routes>
+    </Router>
   );
 }
 
